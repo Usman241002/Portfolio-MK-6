19f3ff355ac7db99db5f01fca93d4c49dd76e521
Usman Khalid <ukhalid428@gmail.com>
2026-09-02 17:40:58 +0100

    created Education controller, as well as all required methods

---
+++ server/src/main/java/dev/ukhalid/portfolio_api/controller/EducationController.java
@@ -0,0 +1,64 @@
+package dev.ukhalid.portfolio_api.controller;
+
+import dev.ukhalid.portfolio_api.model.Education;
+import dev.ukhalid.portfolio_api.repository.EducationRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/education")
+public class EducationController {
+    private final EducationRepository educationRepository;
+
+    public EducationController(final EducationRepository educationRepository) {
+        this.educationRepository = educationRepository;
+    }
+
+    @GetMapping()
+    public Iterable<Education> getAllEducation() {
+        return this.educationRepository.findAll();
+    }
+
+    @PostMapping()
+    public Education createEducation(@RequestBody Education education) {
+        return this.educationRepository.save(education);
+    }
+
+    @PutMapping("/{id}")
+    public Education updateEducation(@PathVariable(name = "id") Integer id, @RequestBody Education education) {
+        Optional<Education> educationToUpdateOptional = this.educationRepository.findById(id);
+
+        if (educationToUpdateOptional.isEmpty()) {
+            return null;
+        }
+
+        Education educationToUpdate = educationToUpdateOptional.get();
+
+        educationToUpdate.setTitle(education.getTitle());
+        educationToUpdate.setInstitution(education.getInstitution());
+        educationToUpdate.setLocation(education.getLocation());
+        educationToUpdate.setStartDate(education.getStartDate());
+        educationToUpdate.setEndDate(education.getEndDate());
+        educationToUpdate.setGrade(education.getGrade());
+        educationToUpdate.setDescription(education.getDescription());
+
+        return this.educationRepository.save(educationToUpdate);
+    }
+
+    @DeleteMapping("/{id}")
+    public Education deleteEducation(@PathVariable(name = "id") Integer id) {
+        Optional<Education> educationToDeleteOptional = this.educationRepository.findById(id);
+
+        if(educationToDeleteOptional.isEmpty()) {
+            return null;
+        }
+
+        Education educationToDelete = educationToDeleteOptional.get();
+        this.educationRepository.delete(educationToDelete);
+
+        return educationToDelete;
+    }
+
+
+}
---
+++ server/src/main/java/dev/ukhalid/portfolio_api/repository/EducationRepository.java
@@ -0,0 +1,7 @@
+package dev.ukhalid.portfolio_api.repository;
+
+import dev.ukhalid.portfolio_api.model.Education;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EducationRepository extends CrudRepository<Education, Integer> {
+}
